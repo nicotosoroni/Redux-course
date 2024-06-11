a3b9619fbb74bcde4590b9435406111750aed06d
Nicolas Francisco Tosoroni <nicolas.tosoroni@intive.com>
2024-06-11 16:13:32 -0300

    add cart functionality, actions and reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Counter from './components/Counter';
 import UpgradeCounter from './components/UpgradeCounter';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <div>
       <h1>useReducer</h1>
+      <hr />
+      <ShoppingCart />
       <hr />
       <Counter />
       <hr />
---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,13 @@
+const ShoppingCart = () => {
+  return (
+    <>
+      <div>Shopping Cart</div>
+      <h3>Products</h3>
+      <article className="box"></article>
+      <h3>Cart</h3>
+      <article className="box"></article>
+    </>
+  );
+};
+
+export default ShoppingCart;
