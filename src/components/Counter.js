7076a6f4715a7379f58d956b2f427546ec987aa4
Nicolas Francisco Tosoroni <nicolas.tosoroni@intive.com>
2024-06-11 10:55:09 -0300

    add counter component with useReducer functionality

---
+++ src/components/Counter.js
@@ -0,0 +1,56 @@
+import { useReducer } from 'react';
+
+const TYPES = {
+  INCREMENT: 'INCREMENT',
+  INCREMENT_5: 'INCREMENT_5',
+  DECREMENT: 'DECREMENT',
+  DECREMENT_5: 'DECREMENT_5',
+  RESET: 'RESET',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case TYPES.INCREMENT:
+      return { count: state.count + 1 };
+    case TYPES.INCREMENT_5:
+      return { count: state.count + action.payload };
+    case TYPES.DECREMENT:
+      return { count: state.count - 1 };
+    case TYPES.DECREMENT_5:
+      return { count: state.count - action.payload };
+    case TYPES.RESET:
+      return initialState;
+    default:
+      return state;
+  }
+}
+
+const initialState = { count: 0 };
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const increment = () => dispatch({ type: TYPES.INCREMENT });
+  const increment5 = () => dispatch({ type: TYPES.INCREMENT_5, payload: 5 });
+  const decrement = () => dispatch({ type: TYPES.DECREMENT });
+  const decrement5 = () => dispatch({ type: TYPES.DECREMENT_5, payload: 5 });
+  const reset = () => dispatch({ type: TYPES.RESET, payload: 0 });
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h2>Counter reducer</h2>
+      <nav>
+        <button onClick={decrement5}>-5</button>
+        <button onClick={decrement}>-</button>
+        <button onClick={increment}>+</button>
+        <button onClick={increment5}>+5</button>
+      </nav>
+      <nav>
+        <button onClick={reset}>Reset</button>
+      </nav>
+      <h3>{state.count}</h3>
+    </div>
+  );
+};
+
+export default Counter;
