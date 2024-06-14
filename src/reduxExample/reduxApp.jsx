37fef080bfdd5587e9ef17d33a21ed74b6807ef2
Nicolas Francisco Tosoroni <nicolas.tosoroni@intive.com>
2024-06-14 11:35:44 -0300

    add all files and folder requiered to build redux app

--- src/reduxExample/reduxApp.jsx
+++ src/reduxExample/reduxApp.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import TeoriaRedux from './components/TeoriaRedux';
 
 function ReduxApp() {
-  return <div>Redux</div>;
+  return (
+    <Provider store={store}>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Redux</h1>
+        <hr />
+        <TeoriaRedux />
+      </div>
+    </Provider>
+  );
 }
 
 export default ReduxApp;
