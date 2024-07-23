6e28780ff49251446e41142069aa1238ea4acb3a
Nicolas Francisco Tosoroni <nicolastosoroni@gmail.com>
2024-07-23 18:16:25 -0300

    adds create, read, and update products slice and logic

--- src/reduxToolkitExample/components/ProductsList.js
+++ src/reduxToolkitExample/components/ProductsList.js
@@ -1,36 +1,105 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { readProducts } from 'reduxToolkitExample/redux/productsSlice';
+import {
+  createProduct,
+  readProduct,
+  updateProduct,
+} from 'reduxToolkitExample/redux/productsSlice';
 
 const ProductsList = () => {
   // @ts-ignore
   const products = useSelector((state) => state.products);
   const dispatch = useDispatch();
+  const [newProductName, setNewProductName] = useState('');
+  const [editedProduct, setEditedProduct] = useState();
 
   useEffect(() => {
     axios
       .get('http://localhost:3001/products')
       .then((res) => {
         console.log(res);
-        dispatch(readProducts(res.data));
+        dispatch(readProduct(res.data));
       })
       .catch((error) => {
         console.log(error);
       });
   }, [dispatch]);
 
+  const handleCreateProduct = () => {
+    if (newProductName) {
+      const newProduct = { id: Date.now(), name: newProductName };
+      dispatch(createProduct(newProduct));
+      axios
+        .post('http://localhost:3001/products', newProduct)
+        .then(() => setNewProductName(''))
+        .catch((error) => console.log(error));
+    }
+  };
+  const handleUpdateProduct = () => {
+    if (editedProduct) {
+      dispatch(
+        // @ts-ignore
+        updateProduct({ id: editedProduct.id, name: editedProduct.name })
+      );
+      axios
+        // @ts-ignore
+        .put(`http://localhost:3001/products/${editedProduct.id}`, {
+          // @ts-ignore
+          name: editedProduct.name,
+        })
+        .then(() => setEditedProduct(null))
+        .catch((error) => console.log('put', error));
+    }
+  };
+  const handleDeleteProduct = () => {};
+
   return (
     <>
       <h3>Products CRUD</h3>
       <ul>
         {products.data.map((product) => (
-          <li key={product.id}>{product.name}</li>
+          <li key={product.id}>
+            {
+              // @ts-ignore
+              editedProduct?.id === product.id ? (
+                <div>
+                  <input
+                    type="text"
+                    value={
+                      // @ts-ignore
+                      editedProduct.name
+                    }
+                    onChange={(e) =>
+                      setEditedProduct({
+                        // @ts-ignore
+                        ...editedProduct,
+                        name: e.target.value,
+                      })
+                    }
+                  />
+                  <button onClick={handleUpdateProduct}>Actualizar</button>
+                </div>
+              ) : (
+                <div>
+                  <span>{product.name}</span>
+                  <button onClick={() => setEditedProduct(product)}>
+                    Editar
+                  </button>
+                  <button onClick={handleDeleteProduct}>Eliminar</button>
+                </div>
+              )
+            }
+          </li>
         ))}
       </ul>
       <aside>
-        <input type="text" />
-        <button>Agregar Producto</button>
+        <input
+          type="text"
+          value={newProductName}
+          onChange={(e) => setNewProductName(e.target.value)}
+        />
+        <button onClick={handleCreateProduct}>Agregar Producto</button>
       </aside>
     </>
   );
--- src/reduxToolkitExample/redux/productsSlice.js
+++ src/reduxToolkitExample/redux/productsSlice.js
@@ -4,16 +4,24 @@ const productsSlice = createSlice({
   name: 'products',
   initialState: { data: [] },
   reducers: {
-    createProducts: (state, action) => {},
-    readProducts: (state, action) => {
+    createProduct: (state, action) => {
+      state.data.push(action.payload);
+    },
+    readProduct: (state, action) => {
       state.data = action.payload;
     },
-    updateProducts: (state, action) => {},
-    deleteProducts: (state, action) => {},
+    updateProduct: (state, action) => {
+      const { id, name } = action.payload;
+      const product = state.data.find((prod) => prod.id === id);
+      if (product) {
+        product.name = name;
+      }
+    },
+    deleteProduct: (state, action) => {},
   },
 });
 
-export const { createProducts, readProducts, updateProducts, deleteProducts } =
+export const { createProduct, readProduct, updateProduct, deleteProduct } =
   productsSlice.actions;
 
 export default productsSlice.reducer;
